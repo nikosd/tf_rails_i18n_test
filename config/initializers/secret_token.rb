3914010505714616bf1e367039bc9a3202aeb64b
Nikos Dimitrakopoulos <n.dimitrakopoulos@pamediakopes.gr>
2011-11-03 12:38:56 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tf18n::Application.config.secret_token = '9426c7db2820ad7fb6dd6f8072ccebe06b11260b2dca4516246fa1402903380fb668ec5e26c7750d6cec3243469aadbe7b49c12c6ced822ca2da4ba5fe61bda9'
